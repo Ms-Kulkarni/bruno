d8087d822ba9a06e91e1fa5852be1894abc1876e
M s kulkarni <mskulkarni7486@gmailn.com>
2025-02-11 18:24:35 +0530

    fixes #3984

--- packages/bruno-app/src/components/CollectionSettings/Tests/StyledWrapper.js
+++ packages/bruno-app/src/components/CollectionSettings/Tests/StyledWrapper.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
-const StyledWrapper = styled.div``;
+
+const StyledWrapper = styled.div`
+  overflow-y: auto;  // Enable vertical scrolling
+  max-height: 100%;  // Ensure it takes up available height
+`;
+
 
 export default StyledWrapper;
